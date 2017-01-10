7401f2ede977c6882294d16bda6f578364c4efb9
fenixisonfire <bizarrebeyondbelief@gmail.com>
2017-01-10 13:50:42 +0000

    login code

---
+++ register-submit.php
@@ -0,0 +1,19 @@
+<?php
+
+$conn = mysqli_connect("localhost", "root", "", "simpsons");
+
+if(!$conn){
+  die("connection failed: ".mysqli_connect_error());
+}
+
+session_start();
+$name = $_POST['name'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$sql = "INSERT INTO students (name, email, password, id)
+VALUES ('$name', '$email', '$password', '')";
+$result = mysqli_query($conn, $sql);
+
+header("Location: start.php");
+?>
