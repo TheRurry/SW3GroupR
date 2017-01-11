50162305eb2e68ea94961e2da50874dbc1e3ca63
Ryan Collins <zcabrdc@ucl.ac.uk>
2017-01-11 22:50:34 +0000

    added additional functionality

---
+++ web/profile_submit.php
@@ -0,0 +1,38 @@
+<?php
+  $conn = mysqli_connect("localhost", "root", "", "simpsons");
+  if(!$conn){
+    die("connection failed: ".mysqli_connect_error());
+  }
+  session_start();
+  $userid = $_POST["userid"];
+  $username = $_POST["username"];
+  $c_pw = $_POST["current_password"];
+  $n_pw = $_POST["new_password"];
+
+  if(is_correct_password($userid, $c_pw)) {
+    if($n_pw == "") {
+      $sql = "UPDATE students SET name='$username' WHERE id='$userid';";
+    } else {
+      $sql = "UPDATE students SET name='$username', password='$n_pw' WHERE id='$userid';";
+    }
+  	$result = mysqli_query($conn, $sql);
+    if(!$result){
+      die("Could not update data: ". mmysql_error());
+    }
+    $_SESSION["name"] = $username;
+  	header("Location: profile.php?userid=".$userid);
+  }
+
+  function is_correct_password($id, $pw) {
+  	$db = new PDO("mysql:dbname=simpsons", "root", "");
+  	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  	$rows = $db->query("SELECT password FROM students WHERE id = '$id';");
+  	foreach ($rows as $row) {
+  		$correct_password = $row["password"];
+  		if ($pw == $correct_password) {
+  			return TRUE;
+  		}
+  	}
+  	return FALSE;
+  }
+?>
---
+++ web/snippet_submit.php
@@ -0,0 +1,14 @@
+<?php
+  $conn = mysqli_connect("localhost", "root", "", "simpsons");
+  if(!$conn){
+    die("connection failed: ".mysqli_connect_error());
+  }
+  session_start();
+  $userid = $_POST["userid"];
+  $title = $_POST["title"];
+  $content = $_POST["content"];
+  $sql = "INSERT INTO snippets (userid, title, content, sid)
+  VALUES ($userid, '$title', '$content', '')";
+  $result = mysqli_query($conn, $sql);
+  header("Location: snippets.php?userid=".$userid);
+?>
