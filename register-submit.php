97de42847e29c08278fa172424a375212cf92a49
fenixisonfire <bizarrebeyondbelief@gmail.com>
2017-01-11 15:48:27 +0000

    register mkdir

--- register-submit.php
+++ register-submit.php
@@ -1,5 +1,5 @@
+#register-submit.php
 <?php
-
 $conn = mysqli_connect("localhost", "root", "", "simpsons");
 
 if(!$conn){
@@ -10,11 +10,14 @@ session_start();
 $name = $_POST['name'];
 $email = $_POST['email'];
 $password = $_POST['password'];
-
 $sql = "INSERT INTO students (name, email, password, id)
 VALUES ('$name', '$email', '$password', '')";
-
 $result = mysqli_query($conn, $sql);
+
+$getId = "SELECT id FROM students WHERE name = '$name';";
+$query = mysqli_query($conn, $getId);
+$newid = mysqli_fetch_array($query);
+mkdir('users/'.$newid['id'],0777,true);
 
 header("Location: start.php");
 ?>
